16ebe6aed44e55e4772846d60e82d3047d7fc381
Przemyslaw Zender <pzender@protonmail.com>
2022-09-05 22:33:17 +1200

    flex-justify to its own class

--- src/app/components/character-summary/character-summary.component.ts
+++ src/app/components/character-summary/character-summary.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class CharacterSummaryComponent implements OnInit {
 
   constructor() { }
+  stats: string[] = ['STR', 'DEX', 'CON', 'INT', 'WIS', 'CHA']
 
   ngOnInit(): void {
   }
